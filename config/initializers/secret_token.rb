cf26cc190d000408301446fc2df42e9c9314644e
peeyush <peeyushagarwal1994@gmail.com>
2014-03-11 23:49:27 +0530

    Barebone rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '3ee6de838343ce29ff91172c4e7812c6f68bab498bf3bf10ec47d85fdab851428188b58c4de691c9fafdd5974322bc4e493a5ac3e3f10ee949ff23a068888775'
